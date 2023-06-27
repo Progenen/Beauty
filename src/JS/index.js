39ba5462ffa7ef0322de8dbabadb5660f38b7431
Stanislav Dombrovsiy PC <stanislav_2010@icloud.com>
2023-06-27 19:18:58 +0500

    Finish

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,29 +1,40 @@
-function validateInput(input) {
+function validInput (input, res, selector) {
     input.addEventListener("input", () => {
-        if (input === '') {
+        if (input.value === res) {
+            input.parentElement.classList.add("form__input-invalid");
+            selector.classList.add("disabled");
             return false;
         } else {
+            input.parentElement.classList.remove("form__input-invalid");
             return true;
         }
     })
 }
 
-function validateEmail(input) {
+function validEmail(input, selector) {
     input.addEventListener("input", () => {
-        if (input.indexOf("@") === -1) {
+        if (input.value.indexOf("@") === -1) {
+            input.parentElement.classList.add("form__input-invalid");
+            selector.classList.add("disabled");
             return false;
         } else {
+            input.parentElement.classList.remove("form__input-invalid");
             return true;
         }
     })
 }
 
-function validateCheckbox(input) {
-    if (input.checked === false) {
-        return false;
-    } else {
-        return true;
-    }
+function validCheckbox(input, res, selector) {
+    input.addEventListener("input", () => {
+        if (input.checked === res) {
+            input.parentElement.classList.add("form__input-invalid");
+            selector.classList.add("disabled");
+            return false;
+        } else {
+            input.parentElement.classList.remove("form__input-invalid");
+            return true;
+        }
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -36,25 +47,130 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (document.querySelector(".access__form")) {
         const accesForm = document.querySelector(".access__form");
-        const formInputs = accesForm.querySelectorAll(".input-value");
-        const accessEmail = accesForm.querySelector("[type='email']");
-        const accessCheckbox = accesForm.querySelectorAll("[type='checkbox']");
-        const accessSubmit = accesForm.querySelector("[type='submit']");
-        accessSubmit.classList.add("disabled");
+        const inputAll = accesForm.querySelectorAll("input")
+        const inputName = accesForm.querySelector("[name='companyName']");
+        const inputIin = accesForm.querySelector("[name='iin']");
+        const inputEmali = accesForm.querySelector("[name='email']");
+        const inputPhone = accesForm.querySelector("[name='phone']");
+        const inputPass = accesForm.querySelector("[name='password']");
+        const inputConfirmPass = accesForm.querySelector("[name='confirmPassword']");
+        const inputOfert = accesForm.querySelector("[name='agreeOfert']");
+        const inputPersonal = accesForm.querySelector('[name="agreePersonal"]');
+        const btn = accesForm.querySelector("[type='submit']");
 
-        accesForm.addEventListener("change", () => {
-            console.log(validateCheckbox(accessCheckbox[0]));
-            if (validateInput(formInputs[0]) && validateInput(formInputs[1]) && validateInput(formInputs[2]) && validateInput(formInputs[3]) && validateInput(formInputs[4])
-            && validateEmail(accessEmail) && validateCheckbox(accessCheckbox[0]) && validateCheckbox(accessCheckbox[1]) 
-            ) {
-                accessSubmit.classList.remove("disabled");
-            } else {
-                accessSubmit.classList.add("disabled");
+        btn.classList.add("disabled");
 
+        validInput(inputName, '', btn);
+        validInput(inputIin, '', btn);
+        validEmail(inputEmali, btn);
+        validInput(inputPhone, '', btn);
+        validInput(inputPass, '', btn);
+        validInput(inputConfirmPass, '', btn);
+        validCheckbox(inputOfert, false, btn);
+        validCheckbox(inputPersonal, false, btn);
+
+        inputAll.forEach(el => {
+                el.addEventListener("input", () => {
+                    if (inputName.value === '') {
+                        inputName.parentElement.classList.remove("form__input-invalid");
+                        btn.classList.add("disabled");
+                    } else if (inputIin.value === '') {
+                        btn.classList.add("disabled");
+                    } else  if (inputEmali.value.indexOf("@") === -1) {
+                        btn.classList.add("disabled");
+                    } else if (inputPhone.value === '') {
+                        btn.classList.add("disabled");
+                    } else if (inputPass.value === '') {
+                        btn.classList.add("disabled");
+                    } else  if (inputConfirmPass.value === '') {
+                        btn.classList.add("disabled");
+                    } else  if (inputOfert.checked === false) {
+                        btn.classList.add("disabled");
+                    } else if (inputPersonal.checked === false) {
+                        btn.classList.add("disabled");
+                    } else {
+                        console.log("succes");
+                        btn.classList.remove("disabled");
+                    }
+             })
+        });
+
+        btn.addEventListener("click", () => {
+            if (inputName.value === '') {
+                inputName.parentElement.classList.add("form__input-invalid");
+            }
+            if (inputIin.value === '') {
+                inputIin.parentElement.classList.add("form__input-invalid");
+            }
+            if (inputEmali.value.indexOf("@") === -1) {
+                inputEmali.parentElement.classList.add("form__input-invalid");
+            }
+            if (inputPhone.value === '') {
+                inputPhone.parentElement.classList.add("form__input-invalid");
+            }
+            if (inputPass.value === '') {
+                inputPass.parentElement.classList.add("form__input-invalid");
+            } 
+            if (inputConfirmPass.value === '') {
+                inputConfirmPass.parentElement.classList.add("form__input-invalid");
+            } 
+            if (inputOfert.checked === false) {
+                inputOfert.parentElement.classList.add("form__input-invalid");
+            }
+            if (inputPersonal.checked === false) {
+                inputPersonal.parentElement.classList.add("form__input-invalid");
+            }
+        })
+    }
+
+    if (document.querySelector(".questions__form")) {
+        const qForm = document.querySelector(".questions__form");
+        const inputsAll = qForm.querySelectorAll("input");
+        const inputName = qForm.querySelector("[name='companyName']");
+        const inputEmali = qForm.querySelector("[name='email']");
+        const inputPersonal = qForm.querySelector('[name="agreePersonal"]');
+        const inputMessage = qForm.querySelector('[name="message"]');
+        const btn = qForm.querySelector("[type='submit']");
+
+        validInput(inputName, '', btn);
+        validEmail(inputEmali, btn);
+        validInput(inputMessage, '', btn);
+        validCheckbox(inputPersonal, false, btn);
+
+        inputsAll.forEach(el => {
+            el.addEventListener("input", () => {
+                if (inputName.value === '') {
+                    btn.classList.add("disabled");
+                } else if (inputEmali.value.indexOf("@") === -1) {
+                    btn.classList.add("disabled");
+                } else if (inputPersonal.checked === false) {
+                    btn.classList.add("disabled");
+                } else if (inputMessage.value === '') {
+                    btn.classList.add("disabled");
+    
+                } else {
+                    btn.classList.remove("disabled");
+                }
+            })
+        });
+
+        btn.classList.add("disabled");
+
+        btn.addEventListener("click", () => {
+            if (inputName.value === '') {
+                inputName.parentElement.classList.add("form__input-invalid");
+            }
+            if (inputEmali.value.indexOf("@") === -1) {
+                inputEmali.parentElement.classList.add("form__input-invalid");
+            }
+            if (inputPersonal.checked === false) {
+                inputPersonal.classList.add("form__input-invalid");
+            }
+            if (inputMessage.value === '') {
+                inputMessage.parentElement.classList.add("form__input-invalid");
             }
         })
 
-        
     }
 
     burger.addEventListener("click", () => {
@@ -100,7 +216,6 @@ document.addEventListener('DOMContentLoaded', function () {
             document.querySelector("#formRecoveryEmailWrong").style.display = "none";
             e.preventDefault();
             if (formRecoveryEmail.value.indexOf("@") <= 0) {
-                console.log("ok");
                 document.querySelector("#formRecoveryEmailWrong").style.display = "block";
             }
             if (formRecoveryAgree.checked === false) {
